b77db5a34e55f2346d5269b4cdcc9d930974cf5d
colmsnowplow <colm@snowplowanalytics.com>
2023-06-21 18:15:32 +0100

    Output the resource ID - different from id which is a url

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
 output "id" {
-  description = "The Storage container ID"
+  description = "The storage container ID"
   value       = azurerm_storage_container.storage_container.id
 }
 
 output "name" {
-  description = "The Storage container name"
+  description = "The storage container name"
   value       = azurerm_storage_container.storage_container.name
+}
+
+output "resource_id" {
+  description = "The storage container resource manager ID"
+  value = azurerm_storage_container.storage_container.resource_manager_id
 }
