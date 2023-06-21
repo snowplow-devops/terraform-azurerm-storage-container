fcc4cb445d8e0af65754836f48dea8387fc0e7d7
colmsnowplow <colm@snowplowanalytics.com>
2023-06-21 18:27:18 +0100

    fmt

--- outputs.tf
+++ outputs.tf
@@ -10,5 +10,5 @@ output "name" {
 
 output "resource_id" {
   description = "The storage container resource manager ID"
-  value = azurerm_storage_container.storage_container.resource_manager_id
+  value       = azurerm_storage_container.storage_container.resource_manager_id
 }
